c528bd16ed98fa5738f4b1f3f74e3c74ddf536bf
MshSiam <siyamkenway@gmail.com>
2022-08-09 08:41:59 +0600

    product page

---
+++ src/pages/Home.js
@@ -0,0 +1,20 @@
+import React from "react"
+import Announcement from "../components/Announcement"
+import Catagories from "../components/Categories"
+import Navbar from "../components/Navbar"
+import Products from "../components/Products"
+import Slider from "../components/Slider"
+
+const Home = () => {
+  return (
+    <div>
+      <Announcement />
+      <Navbar />
+      <Slider />
+      <Catagories />
+      <Products />
+    </div>
+  )
+}
+
+export default Home
