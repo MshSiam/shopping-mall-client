4a7cee14a25e2a2ec13be05d1769246cfeae16b0
MshSiam <siyamkenway@gmail.com>
2022-08-20 11:21:19 +0600

    data upgread

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />}></Route>
-        <Route path="/products/:category" element={<ProductList />}></Route>
+        <Route path="/products" element={<ProductList />}></Route>
         <Route path="/product/:id" element={<Product />}></Route>
         <Route path="/cart" element={<Cart />}></Route>
         <Route path="/login" element={<Login />}></Route>
